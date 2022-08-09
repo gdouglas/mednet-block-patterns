996607a66bd2c0c927d712d560f1772c9262aa61
Graham Douglas <graham.douglas@ubc.ca>
2022-08-09 11:17:49 -0700

    content-card fixes

--- patterns/components/content_card.php
+++ patterns/components/content_card.php
@@ -1,14 +1,22 @@
-<!-- wp:group {"className":"content_card"} -->
-<div class="wp-block-group content_card">
-    <!-- wp:image {"id":474,"sizeSlug":"full","linkDestination":"custom","className":"content-card-image"} -->
-    <figure class="wp-block-image size-full content-card-image">
-        <img
-            src="https://via.placeholder.com/150/002145/ffffff/"
-            alt=""
-            class="wp-image-474"
-        />
-    </figure>
-    <!-- /wp:image -->
+<!-- wp:group {"className":"content-card"} -->
+<div class="wp-block-group content-card">
+    <!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"isDark":false,"className":"content-card-image"} -->
+    <div class="wp-block-cover is-light content-card-image">
+        <span
+            aria-hidden="true"
+            class="wp-block-cover__background has-background-dim"
+        ></span>
+        <div class="wp-block-cover__inner-container">
+            <!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+            <p class="has-text-align-center has-large-font-size">
+                <a href="https://google.ca"
+                    >Add Link here. (Won't display on frontend)</a
+                >
+            </p>
+            <!-- /wp:paragraph -->
+        </div>
+    </div>
+    <!-- /wp:cover -->
 
     <!-- wp:heading {"level":3,"className":"content-card-title"} -->
     <h3 class="content-card-title">
@@ -25,7 +33,7 @@
 
     <!-- wp:paragraph {"className":"content-card-description"} -->
     <p class="content-card-description">
-        Learn more about the Faculty&rsquo;s commitment and actions towards creating
+        Learn more about the Faculty’s commitment and actions towards creating
         respectful and inclusive learning and work environments
     </p>
     <!-- /wp:paragraph -->
